86d8ee7a2c717b0514083c0b87a46354afb2b850
pmjason <157295553+pmjason@users.noreply.github.com>
2026-01-17 17:24:12 +0800

    Delete acticle/ga.js

--- acticle/ga.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-58L8PK1ZWJ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-58L8PK1ZWJ');
-</script>

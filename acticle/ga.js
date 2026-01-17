d2c97ce7809917aff03ae2bbc888abeea6ae1906
pmjason <157295553+pmjason@users.noreply.github.com>
2026-01-17 17:21:14 +0800

    Create ga.js

---
+++ acticle/ga.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-58L8PK1ZWJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-58L8PK1ZWJ');
+</script>
